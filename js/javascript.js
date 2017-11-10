e158d32173da12df85454e889cde652a37389cac
unknown <danielmtalosig@gmail.com>
2017-11-10 19:24:54 +0800

    modifying index part 1

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+		$(window).resize( function(){
+			var width = $('body').width()
+
+			if(width < 500){
+				$("div").find(".media-left").addClass("disable-media-left")
+			location.reload()
+			}
+
+			else{
+				$("div").find(".media-left").removeClass("disable-media-left")	
+			}
+			
+		})
+
+		$(document).ready( function(){
+			var width = $('body').width()
+
+			if(width < 500){
+				$("div").find(".media-left").addClass("disable-media-left")
+			}
+
+			else{
+				$("div").find(".media-left").removeClass("disable-media-left")	
+			}
+		})
